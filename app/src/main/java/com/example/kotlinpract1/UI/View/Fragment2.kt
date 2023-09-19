37189c25e935c3f725486de8aaee88d845d6e041
shevi <shev.igor2002@yandex.ru>
2023-09-19 18:15:30 +0300

    another refactor

--- app/src/main/java/com/example/kotlinpract1/UI/View/Fragment2.kt
+++ app/src/main/java/com/example/kotlinpract1/UI/View/Fragment2.kt
@@ -9,14 +9,13 @@ import androidx.navigation.fragment.findNavController
 import com.example.kotlinpract1.R
 
 class Fragment2 : Fragment() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment2, container, false)
+
         val buttonToFragment1 = view.findViewById<Button>(R.id.button_back_fragment2)
-        // init
 
         buttonToFragment1.setOnClickListener {
             findNavController().navigate(R.id.action_fragment2_to_fragment1)
