6f53c7ff88c7d5c235e3a7544323103752eb9582
shevi <shev.igor2002@yandex.ru>
2023-09-26 20:16:12 +0300

    complete pract7

--- app/src/main/java/com/example/kotlinpract1/UI/View/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpract1/UI/View/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.kotlinpract1.UI.View
 
 import android.annotation.SuppressLint
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
@@ -17,11 +24,18 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_container)
+        //setContentView(R.layout.fragment_container)
 
-        val viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
-        viewModel.data.observe(this, Observer { data ->
-            // обновление UI с новыми данными
-        })
+//        setContent {
+//                // Пример использования Composable элемента с настроенными стилями
+//            StudentInfoUI(
+//                studentName = "Иванов Иван Иванович",
+//                groupName = "Группа 12345",
+//                modifier = Modifier.fillMaxSize().background(Color.White)
+//            )
+//        }
+        setContent {
+            MyApp("Шевелёв Игорь", "ИКБО-25-21")
+        }
     }
 }
